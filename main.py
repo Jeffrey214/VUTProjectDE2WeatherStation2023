29dc5430094bfb5ed8d32768a0e96c5540d44402
Owen <147071765+Brauxo@users.noreply.github.com>
2023-11-16 17:20:12 +0100

    Create main.py
    first version

---
+++ main.py
@@ -0,0 +1,64 @@
+from machine import I2C
+from machine import Pin
+from sh1106 import SH1106_I2C
+import bme280
+import time
+
+# Sensors list
+i2c = I2C(0, scl=Pin(22), sda=Pin(21), freq=100_000)
+
+#display device
+display = SH1106_I2C(128, 64, i2c, addr=0x3c, rotate=180)
+display.contrast(50)  # Set contrast to 50 %
+
+#sensor temp addresses 
+SENSOR_ADDR = 0x5c
+SENSOR_HUMI_REG = 0
+SENSOR_TEMP_REG = 2
+SENSOR_CHECKSUM = 4
+
+#bme280 sensor address and initalization 
+adrbme = 0x76
+bus = smbus2.SMBus(1)
+bmeparams = bme280.load_calibration_params(bus, adrbme)
+
+
+
+print("Stop the code execution by pressing `Ctrl+C` key.")
+print("")
+print("Scanning I2C... ", end="")
+addrs = i2c.scan()
+if SENSOR_ADDR in addrs:
+    print(f"{hex(SENSOR_ADDR)} detected")
+else:
+    print("[ERROR] Sensor is not detected")
+
+try:
+    while True:
+        # readfrom_mem(addr, memaddr, nbytes)
+        vali2c = i2c.readfrom_mem(SENSOR_ADDR, SENSOR_TEMP_REG, 2)
+        
+        display.fill(0)
+        display.text("TEMPERATURE : ", x=0, y=0)
+        display.text(f"{vali2c[0]}.{vali2c[1]} Celcius", x=0, y=10)
+        display.show()
+        time.sleep(5)
+
+        humidity = bme280.sample(bus, adrbme, bmeparamas).humidity
+        
+        display.fill(0)
+        display.text("HUMIDITY: ", x=0, y=0)
+        display.text("{:.2f} g %".format(humidity), x=0, y=10)
+        display.show()
+        time.sleep(5)
+        
+        pressure = bme280.sample(bus, adrbme, bmeparamas).pressure
+        display.fill(0)
+        display.text("PRESSURE : ", x=0, y=0)
+        display.text("{:.2f} Pascal %".format(humidity), x=0, y=10)
+        display.show()
+        time.sleep(5)
+
+
+except KeyboardInterrupt:
+    print("Ctrl+C Pressed. Exiting...")
